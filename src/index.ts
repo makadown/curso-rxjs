78fc9a2b475532458d34cee3add9b2e1bc7c2ffe
Makadown <maseflores79@gmail.com>
2019-12-01 14:50:05 -0600

    Repasando observables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import { Observable } from 'rxjs'
 
+// const obs$ = Observable.create();
+const obs$ = new Observable<string>( subs => {
+    subs.next('Hola');
+    subs.next('Mundo');
 
-console.log('Hola Mundo!');
+    subs.complete();
+// este no se mostrara en log por el complete()
+    subs.next('caca');
+});
 
+obs$.subscribe( console.log );
 
 
 
