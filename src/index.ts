9f2e4c6dbcd1cf1c59a590838938b52c7001be2c
Makadown <maseflores79@gmail.com>
2019-12-02 19:58:57 -0600

    Repasando observer y subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-import { Observable } from 'rxjs'
+import { Observable, Observer } from 'rxjs';
+
+const observer: Observer<any> = {
+    next: value => console.log('siguiente [next]:', value),
+    error: error => console.warn('error [obs]: ', error),
+    complete: () => console.info('Completado [obs]')
+};
 
 // const obs$ = Observable.create();
-const obs$ = new Observable<string>( subs => {
-    subs.next('Hola');
-    subs.next('Mundo');
+const obs$ = new Observable<string>(subs => {
+  subs.next('Hola');
+  subs.next('Mundo');
 
-    subs.complete();
-// este no se mostrara en log por el complete()
-    subs.next('caca');
+  subs.next('Hola');
+  subs.next('Mundo');
+
+  // forzar un error...
+  // const a = undefined;
+  // a.nombre = 'Mayito';
+
+  subs.complete();
+  // este no se mostrara en log por el complete()
+  subs.next('Hola');
+  subs.next('Mundo');
 });
 
-obs$.subscribe( console.log );
+obs$.subscribe(observer);
 
-
-
-
-
-
+// obs$.subscribe( console.log );
+/*obs$.subscribe(
+  valor => console.log('next: ', valor),
+  error => console.warn('error papu!: ', error),
+  () => console.info('Completado!')
+);*/
