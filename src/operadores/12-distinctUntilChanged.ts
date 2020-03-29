262aefb717086a0d962b6896dd98f28a39420fa5
makadown <maseflores79@gmail.com>
2020-03-29 14:09:01 -0600

    Ejercicio distinct untilChanged

--- src/operadores/12-distinctUntilChanged.ts
+++ src/operadores/12-distinctUntilChanged.ts
@@ -1,7 +1,14 @@
 /* ejecicio con operador distinctUntilChanged */
 
 import { of, from } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { distinctUntilChanged, distinct } from 'rxjs/operators';
+
+const numeros$ = of<number | string>(1,'1',1,3,3,2,2,4,4,5,3,1,'1');
+
+numeros$.pipe(
+    distinctUntilChanged()
+)
+.subscribe(console.log);
 
 interface Personaje {
     nombre: string;
